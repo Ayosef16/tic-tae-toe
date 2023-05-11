c09868c262c4cf113c6d0b33d62125c20efeaf31
Ayosef <syrivera@uc.cl>
2023-05-11 10:57:38 -0400

    Add basic layout and gameboard

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameBoard = () => {
+}
+
+let gameboard = [['x','x','x'], ['x','x','x'], ['x','x','x']];
+
+let player1 = {
+    name: 'Ben',
+    mark: 'O'
+}
+
+let player2 = {
+    name: 'Sophie',
+    mark: 'X'
+}
+
+function putMark() {
+    
+}
