550a58e278c06ef883399dd3e084cb2074dd6710
Ayosef <syrivera@uc.cl>
2023-05-12 11:10:14 -0400

    Reorganize the funcstion into modules and change the board size

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-const gameCell = document.querySelectorAll('td');
+//function factory to create players
 
-const gameBoard = () => {
+const playerFactory = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    return { getName, getMark };
 }
 
-let gameboard = ['','','','','','','','',''];
-
-let player1 = {
-    name: 'Ben',
-    mark: 'X'
-}
-
-let player2 = {
-    name: 'Sophie',
-    mark: 'O'
-}
+let player1 = playerFactory('Ben', 'X');
+let player2 = playerFactory('Marta', 'O');
 
 let player = player1;
 
-function changePlayer() {
-    if (player === player1) {
-        player = player2;
+//create the gameBoard module
+
+const gameBoard = (() => {
+    const gameboard = ['','','','','','','','',''];
+    const setIndex = (boardIndex) => {
+        gameboard[boardIndex] = player.getMark();
     }
-    else if (player === player2) {
-        player = player1;
-    }
-}
+    const showBoard = () => gameboard;
+    return { setIndex, showBoard }
+})();
+
+//create the displayController module
+
+const displayController = (() => {
+    const gameCell = document.querySelectorAll('td');
+
+    const changePlayer = () => {
+        if (player === player1) {
+            player = player2;
+        }
+        else if (player === player2) {
+            player = player1;
+        }
+    };
+
+    gameCell.forEach(cell =>  cell.addEventListener('click', (e) => {
+        console.log(e.currentTarget.dataset.number);
+        let index = e.currentTarget.dataset.number - 1;
+        e.currentTarget.textContent = player.getMark();
+        gameBoard.setIndex(index);
+        changePlayer();
+    }));
+})();
 
 
-gameCell.forEach(cell =>  cell.addEventListener('click', (e) => {
-    console.log(e.currentTarget.dataset.number);
-    let number = e.currentTarget.dataset.number - 1;
-    e.currentTarget.textContent = player.mark;
-    gameboard[number] = player.mark;
-    changePlayer();
-}));
+
