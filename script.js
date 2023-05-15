1749dbf91474a1cfcf8f17af8ea9d22883d4a1df
Ayosef <syrivera@uc.cl>
2023-05-15 10:14:16 -0400

    Add the win conditions to the board

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ const playerFactory = (name, mark) => {
 let player1 = playerFactory('Ben', 'X');
 let player2 = playerFactory('Marta', 'O');
 
-let player = player1;
 
 //create the gameBoard module
 
 const gameBoard = (() => {
     const gameboard = ['','','','','','','','',''];
     const setIndex = (boardIndex) => {
-        gameboard[boardIndex] = player.getMark();
+        gameboard[boardIndex] = displayController.getPlayer().getMark();
     }
     const showBoard = () => gameboard;
     return { setIndex, showBoard }
@@ -26,24 +25,93 @@ const gameBoard = (() => {
 
 const displayController = (() => {
     const gameCell = document.querySelectorAll('td');
+    const winner = document.querySelector('.winner');
 
-    const changePlayer = () => {
-        if (player === player1) {
-            player = player2;
+    let turn = 1;
+    let player;
+
+    const playerTurn = () => {
+        return turn % 2 === 0 ? player = player2 : player = player1;
+    }
+/*
+    const checkWinner = () => {
+        //check rows
+        for (let i=0; i<9; i+=3) {
+            if (gameBoard.showBoard()[i] !== '' && gameBoard.showBoard()[i] === gameBoard.showBoard()[i+1] && gameBoard.showBoard()[i] === gameBoard.showBoard()[i+2]) {
+                if (gameBoard.showBoard()[i] === 'X') {
+                    return `${player1.getName()} wins!`
+                }
+                else {
+                    return `${player2.getName()} wins!`
+                }
+            }
         }
-        else if (player === player2) {
-            player = player1;
+        //check columns
+        for (let i=0; i<3; i++) {
+            if (gameBoard.showBoard()[i] !== '' && gameBoard.showBoard()[i] === gameBoard.showBoard()[i+3] && gameBoard.showBoard()[i] === gameBoard.showBoard()[i+6]) {
+                if (gameBoard.showBoard()[i] === 'X') {
+                    return `${player1.getName()} wins!`
+                }
+                else {
+                    return `${player2.getName()} wins!`
+                }
+            }
         }
-    };
+        //check diagonals
+        if(gameBoard.showBoard()[0] !== '' && gameBoard.showBoard()[0] === gameBoard.showBoard()[4] && gameBoard.showBoard()[0] === gameBoard.showBoard()[8]) {
+            if (gameBoard.showBoard()[0] === 'X') {
+                return `${player1.getName()} wins!`
+            }
+            else {
+                return `${player2.getName()} wins!`
+            }
+        }
+        if(gameBoard.showBoard()[2] !== '' && gameBoard.showBoard()[2] === gameBoard.showBoard()[4] && gameBoard.showBoard()[2] === gameBoard.showBoard()[6]) {
+            if (gameBoard.showBoard()[0] === 'X') {
+                return `${player1.getName()} wins!`
+            }
+            else {
+                return `${player2.getName()} wins!`
+            }
+        }
+    }*/
+
+    const checkWinner = (board) => {
+        const winCondition = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for (let win of winCondition) {
+            if (board[win[0]] !== '' && board[win[0]] === board[win[1]] && board[win[0]] === board[win[2]]) {
+                return declareWinner();
+            }
+        }
+    }
+
+    const declareWinner = () => {
+        return `${player.getName()} wins!`
+    }
+
+    const getPlayer = () => player;
 
     gameCell.forEach(cell =>  cell.addEventListener('click', (e) => {
         if (e.currentTarget.textContent !== '') return
-        console.log(e.currentTarget.dataset.number);
+        if (winner.textContent !== '') return
+        playerTurn();
         let index = e.currentTarget.dataset.number - 1;
         e.currentTarget.textContent = player.getMark();
         gameBoard.setIndex(index);
-        changePlayer();
+        turn++;
+        winner.textContent = checkWinner(gameBoard.showBoard());
     }));
+
+    return { getPlayer }
 })();
 
 
